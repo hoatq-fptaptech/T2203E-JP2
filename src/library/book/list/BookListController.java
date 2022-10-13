67b0271b1b2c47fb2be5f599dfd4b03be9603187
billytrinh <quanghoa.trinh@gmail.com>
2022-10-13 20:02:03 +0700

    first commit

---
+++ src/library/book/list/BookListController.java
@@ -0,0 +1,34 @@
+package library.book.list;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import library.entities.Book;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BookListController implements Initializable {
+    public TableView<Book> tbBooks;
+    public TableColumn<Book,Integer> tdId;
+    public TableColumn<Book,String> tdName;
+    public TableColumn<Book,String> tdAuthor;
+    public TableColumn<Book,Integer> tdQty;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tdId.setCellValueFactory(new PropertyValueFactory<Book,Integer>("id"));
+        tdName.setCellValueFactory(new PropertyValueFactory<Book,String>("name"));
+        tdAuthor.setCellValueFactory(new PropertyValueFactory<Book,String>("author"));
+        tdQty.setCellValueFactory(new PropertyValueFactory<Book,Integer>("qty"));
+
+        ObservableList<Book> ls = FXCollections.observableArrayList();
+        ls.add(new Book(1,"Trí tuệ Do Thái","ABC",10));
+        ls.add(new Book(2,"Trí tuệ VN","XYZ",20));
+
+        tbBooks.setItems(ls);
+    }
+}
