3265ff2cb78e08de22fb1dd3b9036bbc3dc1441c
billytrinh <quanghoa.trinh@gmail.com>
2022-10-22 16:45:26 +0700

    generic

---
+++ src/demogeneric/Generic.java
@@ -0,0 +1,26 @@
+package demogeneric;
+
+public class Generic <X,Y>{
+    X dataX;
+    Y dataY;
+
+    public X getDataX() {
+        return dataX;
+    }
+
+    public void setDataX(X dataX) {
+        this.dataX = dataX;
+    }
+
+    public Y getDataY() {
+        return dataY;
+    }
+
+    public void setDataY(Y dataY) {
+        this.dataY = dataY;
+    }
+
+    public <A> void run(A msg){ // generic in method
+        System.out.println(msg);
+    }
+}
---
+++ src/demogeneric/SubGeneric.java
@@ -0,0 +1,5 @@
+package demogeneric;
+
+public class SubGeneric<String,Y> extends Generic<String,Y>{
+
+}
